c32773a4cf32c16ee72e8e6504746a510ade2db6
SylvaticGhost <k.tkachov05@gmail.com>
2024-07-14 19:11:19 +0300

    add creating and closing account

--- Sql/tables.sql
+++ Sql/tables.sql
@@ -8,3 +8,28 @@ CREATE TABLE user_schema.users (
     password_hash text,
     password_salt text
 );
+
+CREATE TABLE account_schema.accounts (
+    id uuid PRIMARY KEY,
+    owner_id uuid,
+    created_at timestamp,
+    currency text,
+    type text,
+    FOREIGN KEY (owner_id) REFERENCES user_schema.users(id)
+);
+
+CREATE TABLE account_schema.operations (
+    transaction_id uuid,
+    account_id uuid,
+    partner_id uuid,
+    amount numeric,
+    currency text,
+    time timestamp,
+    type text,
+    comment text,
+    PRIMARY KEY (transaction_id, account_id),
+    FOREIGN KEY (account_id) REFERENCES account_schema.accounts(id),
+    FOREIGN KEY (partner_id) REFERENCES account_schema.accounts(id)
+);
+
+CREATE INDEX operations_account_id_idx ON account_schema.operations(account_id);
---
+++ src/operation/operation.entity.ts
@@ -0,0 +1,10 @@
+export class Operation {
+    transaction_id: string;
+    accountId: string;
+    partnerId: string;
+    amount: number;
+    currency: string;
+    time: Date;
+    type: string;
+    comment: string;
+}
