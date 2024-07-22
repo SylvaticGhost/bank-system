38af35258f68f4f1f8d5dea5e451772dcb590d75
SylvaticGhost <143171907+SylvaticGhost@users.noreply.github.com>
2024-07-22 11:08:13 +0300

    add importing data

---
+++ scripts/import-data.js
@@ -0,0 +1,34 @@
+const { PrismaClient } = require('@prisma/client');
+const fs = require('fs-extra');
+require('dotenv').config();
+
+const prismaClient = new PrismaClient({
+    datasources: { db: { url: process.env.DATABASE_URL } }
+});
+
+async function importData() {
+    console.log('Importing data...')
+    try {
+        const users = await fs.readJson('users.json')
+        await prismaClient.user.createMany({ data: users })
+        
+        const accounts = await fs.readJson('accounts.json')
+        await prismaClient.account.createMany({ data: accounts })
+        
+        const operations = await fs.readJson('operations.json')
+        await prismaClient.operation.createMany({ data: operations })
+        
+        const blockedUsers = await fs.readJson('blockedUsers.json')
+        await prismaClient.blockedUser.createMany({ data: blockedUsers })
+        
+        console.log('Data imported successfully!')
+    }
+    catch (error) {
+        console.error(error)
+    }
+    finally {
+        await prismaClient.$disconnect()
+    }
+}
+
+importData()
